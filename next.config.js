874750b73ba63c2d416b45f46930e7e647c685a8
FacundoCastillo12 <facuelfacha15@gmail.com>
2023-03-13 22:02:50 -0300

    add dependencies and delete unnecessary files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/rooms/:id",
+        destination: "/rooms/[id]",
+      },
+    ];
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
